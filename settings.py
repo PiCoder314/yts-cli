32969643e20e09d337cb6bff7c2bbb6d641fb6c8
PiCoder314 <avguy1001@gmail.com>
2019-10-03 21:57:11 +0530

    updated settings variables

--- settings.py
+++ settings.py
@@ -15,4 +15,5 @@ HOME_LINK = 'https://yts.lt/browse-movies/Bumblebee/all/all/0/latest'
 DOWNLOAD_LINK = 'https://yts.lt/torrent/download'
 TITLE_CLASS = 'browse-movie-title'
 YEAR_CLASS = 'browse-movie-year'
+OPEN_COMMAND = 'xdg-open tmp.torrent'
 PROXIES = {}
