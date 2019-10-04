aaba21fc89430faf1b8b6c0570bf26df4e4e917f
PiCoder314 <avguy1001@gmail.com>
2019-10-04 15:32:22 +0530

    Code Cleanup

--- settings.py
+++ settings.py
@@ -1,15 +1,3 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-# Copyright © 2019  <@localhost>
-#
-# Distributed under terms of the MIT license.
-
-"""
-settings for yts-cli
-"""
-
 SEARCH_LINK = 'https://yts.lt/browse-movies/{query}/all/all/0/latest'
 HOME_LINK = 'https://yts.lt/browse-movies/Bumblebee/all/all/0/latest'
 DOWNLOAD_LINK = 'https://yts.lt/torrent/download'
